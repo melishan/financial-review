79391e45ed7bb6fcf715a34a146beb02ab41c804
melishan <melishan@gmail.com>
2018-05-09 09:20:35 +0300

    updated script

--- mainScript.R
+++ mainScript.R
@@ -1,6 +1,6 @@
 #Observing the data
 getwd()
-setwd("/Users/melis/Desktop/R")
+setwd("/Users/melis/Desktop/R/financialReviewUpdated")
 getwd()
 
 fin <- read.csv("Future-500.csv")
@@ -19,7 +19,21 @@ summary(fin)
 str(fin)
 summary(fin) 
 
-#Factor Variable Trap 
+#characters to numerics
 a <- c("12","13","14", "12", "12")
 a
+typeof(a)
+b <- as.numeric(a)
+typeof(b)
+b
+#numerics to factor - factor is used to categorized vectors
+m <- factor(a)
+m
+z  <- as.numeric(m) #converting factors to numerics. It shows categories as numerics so we have 3 categories and 3 numbers.
+z #if you want to get categories names then don't convert factors to numbers like we did in this example.,
 
+#Converting fin dataset to the right type of variables.
+#sub() gsub()
+fin$Expenses <- gsub(" Dollars", "", fin$Expenses)
+fin$Expenses <- gsub(",", "", fin$Expenses)
+head(fin)
